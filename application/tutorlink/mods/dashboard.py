450e33ff8778dade2b8d772ed3e64cfaae7c7379
Burned357Waffles <bwatanabe@sfsu.edu>
2023-11-14 19:52:16 -0800

    updated messages and added comment

--- application/tutorlink/mods/dashboard.py
+++ application/tutorlink/mods/dashboard.py
@@ -8,4 +8,6 @@ from flask import render_template
 # # # Routes
 @app.route("/dashboard", methods=['GET'])
 def dashboard():
-    return render_template('dashboard.jinja2', my_posts=Tutor.query.all(), sent=Tutor.query.all(), received=Tutor.query.all())
+    # TODO: change my_posts, sent, and received to be the correct data
+    return render_template('dashboard.jinja2', my_posts=Tutor.query.all(), sent=Tutor.query.all(),
+                           received=Tutor.query.all())

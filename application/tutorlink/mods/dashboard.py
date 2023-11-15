cfdc446c6c691ddfc8121a7ad40f45f3bc32979f
Burned357Waffles <bwatanabe@sfsu.edu>
2023-11-14 20:13:16 -0800

    created new partial for "My Posts" tab

--- application/tutorlink/mods/dashboard.py
+++ application/tutorlink/mods/dashboard.py
@@ -10,4 +10,4 @@ from flask import render_template
 def dashboard():
     # TODO: change my_posts, sent, and received to be the correct data
     return render_template('dashboard.jinja2', my_posts=Tutor.query.all(), sent=Tutor.query.all(),
-                           received=Tutor.query.all())
+                           received=Tutor.query.all(), subj_db=Subject)

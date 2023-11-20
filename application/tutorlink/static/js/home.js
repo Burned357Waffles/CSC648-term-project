24f318312d2cc2509f7d1ace5ea79a28627eaf67
Burned357Waffles <burned357waffles@protonmail.com>
2023-11-20 13:31:05 -0800

    updated comments

--- application/tutorlink/static/js/home.js
+++ application/tutorlink/static/js/home.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
-  // Click event for rectangles within the carousel
   $('.rectangle').on('click', function() {
-    // Get the text inside the clicked rectangle
     var subjectText = $(this).text().trim();
 
-    // Redirect to a new page passing the subject text as a query parameter
+    // Redirect to the search page for the subject clicked
     window.location.href = '/search/' + encodeURIComponent(subjectText);
-    // Replace '/new-page' with the desired page URL
   });
 });
 

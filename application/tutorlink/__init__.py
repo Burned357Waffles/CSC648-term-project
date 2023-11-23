d0339b6a1f81ea88b1d168856eab971cfd85bfd5
Burned357Waffles <burned357waffles@protonmail.com>
2023-11-22 19:11:54 -0800

    carousel removed and replaced with card
    also fixed posts on home and search page to be properly centered

--- application/tutorlink/__init__.py
+++ application/tutorlink/__init__.py
@@ -166,6 +166,46 @@ if app.debug:
                 tutor_vid="https://youtu.be/dQw4w9WgXcQ?si=tCj8boDIu7EYF342",
                 tutor_subj=1,
                 tutor_subj_num="4321"
+            ),
+            Tutor(
+                tutor_user="fakeuser",
+                tutor_name="Ulfric Stormcloak",
+                tutor_bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                tutor_cv="image/test/li.pdf",
+                tutor_photo="image/test/def_pfp.jpg",
+                tutor_vid="https://youtu.be/dQw4w9WgXcQ?si=tCj8boDIu7EYF342",
+                tutor_subj=5,
+                tutor_subj_num="4201"
+            ),
+            Tutor(
+                tutor_user="fakeuser",
+                tutor_name="Tiber Septim",
+                tutor_bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                tutor_cv="image/test/li.pdf",
+                tutor_photo="image/test/def_pfp.jpg",
+                tutor_vid="https://youtu.be/dQw4w9WgXcQ?si=tCj8boDIu7EYF342",
+                tutor_subj=4,
+                tutor_subj_num="2828"
+            ),
+            Tutor(
+                tutor_user="fakeuser",
+                tutor_name="Gideon Ofnir",
+                tutor_bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                tutor_cv="image/test/li.pdf",
+                tutor_photo="image/test/def_pfp.jpg",
+                tutor_vid="https://youtu.be/dQw4w9WgXcQ?si=tCj8boDIu7EYF342",
+                tutor_subj=6,
+                tutor_subj_num="101"
+            ),
+            Tutor(
+                tutor_user="fakeuser",
+                tutor_name="Miles Davis",
+                tutor_bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                tutor_cv="image/test/li.pdf",
+                tutor_photo="image/test/def_pfp.jpg",
+                tutor_vid="https://youtu.be/dQw4w9WgXcQ?si=tCj8boDIu7EYF342",
+                tutor_subj=6,
+                tutor_subj_num="1296"
             )
         ]
         for i in tutors:
--- application/tutorlink/static/js/home.js
+++ application/tutorlink/static/js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $('.rectangle').on('click', function() {
-    var subjectText = $(this).text().trim();
+  $('.subject-card').on('click', function() {
+    const subjectText = $(this).text().trim();
 
     // Redirect to the search page for the subject clicked
     window.location.href = '/search/' + encodeURIComponent(subjectText);

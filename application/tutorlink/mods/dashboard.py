691078cd3a0ad27a5e23992d448ad6f6dd5a159f
Abel <abel.e.seyoum@gmail.com>
2023-12-08 17:58:26 -0800

    made messages appear in chronological order

--- application/tutorlink/mods/dashboard.py
+++ application/tutorlink/mods/dashboard.py
@@ -16,9 +16,12 @@ def dashboard():
     # Query database for user info to populate dashboard tabs with
     user = current_user.user_name  # TODO: remove this for method mentioned in dashboard.jinja2
     my_posts = Tutor.query.filter(Tutor.tutor_user == current_user.user_id).all()
-    sent = Message.query.filter(Message.msg_student == current_user.user_id).all()
-    received = Message.query.filter(Message.msg_tutor == current_user.user_id).all()
 
-    # TODO: change my_posts, sent, and received to be the correct data
+    # Get user's sent/received messages in order of (most recent -> oldest)
+    sent = Message.query.filter(Message.msg_student == current_user.user_id)
+    sent = sent.order_by(Message.msg_id.desc()).all()
+    received = Message.query.filter(Message.msg_tutor == current_user.user_id)
+    received = received.order_by(Message.msg_id.desc()).all()
+
     return render_template('dashboard.jinja2', user=user, my_posts=my_posts, sent=sent,
                            received=received, tutor_db=Tutor, subj_db=Subject)

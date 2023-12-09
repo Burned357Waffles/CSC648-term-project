84ab75dfb0e3b694686e481ee442f9485f9e5672
Abel <abel.e.seyoum@gmail.com>
2023-12-08 17:53:39 -0800

    added username

--- application/tutorlink/mods/dashboard.py
+++ application/tutorlink/mods/dashboard.py
@@ -14,10 +14,11 @@ def dashboard():
         return redirect(url_for("login_page"))
 
     # Query database for user info to populate dashboard tabs with
+    user = current_user.user_name  # TODO: remove this for method mentioned in dashboard.jinja2
     my_posts = Tutor.query.filter(Tutor.tutor_user == current_user.user_id).all()
     sent = Message.query.filter(Message.msg_student == current_user.user_id).all()
     received = Message.query.filter(Message.msg_tutor == current_user.user_id).all()
 
     # TODO: change my_posts, sent, and received to be the correct data
-    return render_template('dashboard.jinja2', my_posts=my_posts, sent=sent,
+    return render_template('dashboard.jinja2', user=user, my_posts=my_posts, sent=sent,
                            received=received, tutor_db=Tutor, subj_db=Subject)
